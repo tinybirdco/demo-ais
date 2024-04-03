23a61d04eeaf668235cb6d28e9f287ca9aee9002
Dan Chaffelson <chaffelson@gmail.com>
2024-04-03 15:46:56 +0300

    Added multiple H3 resolutions

--- app.py
+++ app.py
@@ -45,10 +45,11 @@ def prepare_hexagon_data(df, h3_boundary_column_name):
     for boundary in df[h3_boundary_column_name]:
         # Ensure the polygon is closed by appending the first vertex to the end
         boundary.append(boundary[0])
-        
-        # Unpack the array of [lat, lon] pairs into separate lists,
-        # reversing the order to [lon, lat] if necessary for your mapping tool
-        lons, lats = zip(*[(lon, lat) for lat, lon in boundary])
+        # Reverse the order of vertices so they render correctly in plotly.
+        boundary_reversed = boundary[::-1]
+        # Unpack the array of [lat, lon] pairs into separate lists
+        # reversing the order to [lon, lat] for plotly
+        lons, lats = zip(*[(lon, lat) for lat, lon in boundary_reversed])
         
         hexagon_data.append({'lon': lons, 'lat': lats})
     return hexagon_data
@@ -69,10 +70,12 @@ app.layout = html.Div([
         dcc.RadioItems(
             id='display-mode',
             options=[
-                {'label': 'Show LatLong Points', 'value': 'data_points'},
-                {'label': 'Show H3 Hexagons', 'value': 'h3_hexagons'},
+                {'label': 'H3 r4', 'value': 'h3_r4'},
+                {'label': 'H3 r6', 'value': 'h3_r6'},
+                {'label': 'H3 r8', 'value': 'h3_r8'},
+                {'label': 'LatLong', 'value': 'data_points'}
             ],
-            value='h3_hexagons',  # Default value
+            value='h3_r4',  # Default value
             labelStyle={'display': 'block'}  # Display options in block mode
         ),
         html.H4('Select MMSI:', style={'padding': '5px'}),
@@ -120,110 +123,85 @@ def prepare_performance_info(cached_data):
 
     return [
         html.Div(f"Request roundtrip time: {request_roundtrip_time:.3f} s"),
-        html.Div(f"Tinybird processing time: {tb_elapsed} ms"),
+        html.Div(f"Tinybird processing time: {tb_elapsed:.3f} ms"),
         html.Div(f"Tinybird Bytes read: {format_bytes(tb_bytes_read)}"),
         html.Div(f"Data points: {data_points}")
     ]
 
 @app.callback(
     Output('cached-data', 'data'),
-    [Input('submit-val', 'n_clicks'), Input('display-mode', 'value')],
-    [State('mmsi-dropdown', 'value'), State('start-date-input', 'date'), State('end-date-input', 'date')]
+    [Input('submit-val', 'n_clicks')],
+    [State('mmsi-dropdown', 'value'), 
+     State('start-date-input', 'date'), 
+     State('end-date-input', 'date'), 
+     State('display-mode', 'value')]
 )
-def fetch_data(n_clicks, display_mode, mmsi, start_date, end_date):
+def fetch_data(n_clicks, mmsi, start_date, end_date, display_mode):
     if n_clicks > 0:
         start_request_time = time.time()
-        
-        if display_mode == 'data_points':
-            url = f'{TB_BASE_URL}latlon_by_date_by_mmsi.json?mmsis={mmsi}&startdate={start_date}&enddate={end_date}&token={TB_TOKEN}'
-        elif display_mode == 'h3_hexagons':
-            url = f'{TB_BASE_URL}h3_r6_by_date_by_mmsi.json?mmsis={mmsi}&startdate={start_date}&enddate={end_date}&token={TB_TOKEN}'
+        api_endpoint = "latlon_by_date_by_mmsi.json" if display_mode == 'data_points' else "h3_by_date_by_mmsi.json"
+        h3_resolution = "" if display_mode == 'data_points' else f"&h3r=h3_r{display_mode[-1]}"
+        url = f"{TB_BASE_URL}{api_endpoint}?mmsis={mmsi}&startdate={start_date}&enddate={end_date}{h3_resolution}&token={TB_TOKEN}"
         
         response = requests.get(url)
         end_request_time = time.time()
-
-        # Calculate request roundtrip time and package the data
-        data = {
+        print(f"Requests data length: {len(response.json()['data'])}")
+        
+        return {
             'mode': display_mode,
-            'data': response.json()['data'],  # Assuming the API response has a 'data' key
+            'data': response.json()['data'],
             'request_roundtrip_time': end_request_time - start_request_time,
-            'statistics': response.json().get('statistics', {})  # Include additional response metadata if available
+            'statistics': response.json().get('statistics', {}),
+            'cache_key': f"{display_mode}_{start_date}_{end_date}_{mmsi}"  # Unique key for caching
         }
-
-        return data
     return {}
-
-def update_geos_viewport(fig, lats, lons):
-    """
-    Update the map's viewport based on latitude and longitude bounds.
-    """
-    min_lat, max_lat = min(lats), max(lats)
-    min_lon, max_lon = min(lons), max(lons)
-
-    fig.update_geos(
-        projection_type="equirectangular",
-        visible=False, 
-        showcountries=True, 
-        lonaxis=dict(range=[min_lon - 0.1, max_lon + 0.1]),  # Adding a small margin
-        lataxis=dict(range=[min_lat - 0.1, max_lat + 0.1])
-    )
-
-    fig.update_layout(geo=dict(fitbounds="locations"))
 
 @app.callback(
     [Output('map-display', 'figure'), Output("performance-info", "children")],
-    [Input('cached-data', 'data'), Input('display-mode', 'value')]
+    [Input('cached-data', 'data')]
 )
-def update_map(cached_data, display_mode):
+def update_map(cached_data):
     if not cached_data or 'data' not in cached_data:
         return go.Figure(), "Select options and click submit to see data."
 
+    display_mode = cached_data.get('mode')
+    print(f"Dislay mode: {display_mode}")
+    print(f"Cache Key: {cached_data.get('cache_key')}")
     df = pd.DataFrame(cached_data['data'])
     fig = go.Figure()
 
-    lats, lons = [], []
-
-    if display_mode == 'h3_hexagons':
-        # Preparing hexagon data
-        hexagon_data = prepare_hexagon_data(df, 'h3_boundary')
-        for hex_data in hexagon_data:
-            fig.add_trace(go.Scattergeo(
-                lon=hex_data['lon'], 
-                lat=hex_data['lat'], 
-                mode='lines',
-                line=dict(width=1, color='orange'),  # Hexagon line color
-                fill='toself',
-                fillcolor='rgba(255, 165, 0, 0.5)'  # Hexagon fill color with some transparency
-            ))
-            lats.extend(hex_data['lat'])
-            lons.extend(hex_data['lon'])
-
+    # Rendering logic based on display_mode
+    if display_mode.startswith('h3'):
+        # Render hexagons
+        fig = render_hexagons(df, fig)
     elif display_mode == 'data_points':
-        # Plotting lat-long points with specific marker settings
-        fig.add_trace(go.Scattergeo(
-            lon=df['lon'], 
-            lat=df['lat'], 
-            mode='markers+lines',
-            marker=dict(size=7, color='red'),  # Data point color
-            line=dict(width=2, color='red'),  # Line color connecting the points
-            text=df['basedatetime'],  # Assuming 'basedatetime' contains timestamp or similar info
-            name='Path'
-        ))
-        lats = df['lat'].tolist()
-        lons = df['lon'].tolist()
+        # Render lat/long points
+        fig = render_latlong_points(df, fig)
 
-    # Adjusting the map's viewport based on the newly loaded data
-    update_geos_viewport(fig, lats, lons)
+    # Adjust viewport to fit the rendered data
+    adjust_viewport(fig)
 
+    performance_info = prepare_performance_info(cached_data)
+    return fig, performance_info
+
+def adjust_viewport(fig):
+    # Attempt to automatically adjust the map's viewport to show all plotted data
+    fig.update_layout(
+        geo=dict(
+            fitbounds="locations",  # This tells Plotly to fit the plotted locations
+            projection_type="equirectangular"
+        ),
+        margin={"r":0, "t":0, "l":0, "b":0}
+    )
     # Updating layout to include custom styling
     fig.update_layout(
         margin={"r":0, "t":0, "l":0, "b":0},
         geo=dict(
             scope='world',
             showland=True,
-            landcolor="LightGreen",
+            landcolor="rgb(217, 217, 217)",  # This sets the land color
             showocean=True,
-            oceancolor="LightBlue",
+            oceancolor="rgb(224, 255, 255)",  # This sets the ocean color
             showcountries=True,
             countrycolor="DarkGrey",
             fitbounds="locations"  # Automatically adjusting viewport to fit the data
@@ -231,9 +209,32 @@ def update_map(cached_data, display_mode):
         title=dict(text="AIS Data Visualization", x=0.5)  # Center title
     )
 
-    performance_info = prepare_performance_info(cached_data)
-    
-    return fig, performance_info
+def render_hexagons(df, fig):
+    # Preparing hexagon data
+    hexagon_data = prepare_hexagon_data(df, 'h3_boundary')
+    for hex_data in hexagon_data:
+        fig.add_trace(go.Scattergeo(
+            lon=hex_data['lon'], 
+            lat=hex_data['lat'], 
+            mode='lines',
+            line=dict(width=1, color='orange'),  # Hexagon line color
+            fill='toself',
+            fillcolor='rgba(255, 165, 0, 0.5)'  # Hexagon fill color with some transparency
+        ))
+    return fig
+
+def render_latlong_points(df, fig):
+    # Plotting lat-long points with specific marker settings
+    fig.add_trace(go.Scattergeo(
+        lon=df['lon'], 
+        lat=df['lat'], 
+        mode='markers+lines',
+        marker=dict(size=7, color='red'),  # Data point color
+        line=dict(width=2, color='red'),  # Line color connecting the points
+        text=df['basedatetime'],  # Assuming 'basedatetime' contains timestamp or similar info
+        name='Path'
+    ))
+    return fig
 
 
 if __name__ == '__main__':
